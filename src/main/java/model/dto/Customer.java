0648d01150b1c1df5a61fea91d29ade40d673260
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-04-03 11:06:25 -0300

    create model/dto/Customer.java

--- src/main/java/model/dto/Customer.java
+++ src/main/java/model/dto/Customer.java
@@ -1,7 +1,15 @@
 package model.dto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Customer {
+
+    @Id
+    private Integer id;
+
+    @Column(name = "customer_name")
+    private String name;
 }

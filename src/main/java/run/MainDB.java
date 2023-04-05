b28bf3c0273960000bdffe5938235062d5915326
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-04-05 12:17:55 -0300

    initiates a main file to test db connection

---
+++ src/main/java/run/MainDB.java
@@ -0,0 +1,11 @@
+package run;
+
+import model.dto.Order;
+import model.dto.Product;
+
+public class MainDB {
+    public static void main(String[] args) {
+        Order pedido = new Order();
+        Product produto = new Product();
+    }
+}

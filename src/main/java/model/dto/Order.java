fecd50028737f68b90543347711d44889f641f3c
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-04-06 18:18:09 -0300

    delete lombok to fix incompatibility issues

--- src/main/java/model/dto/Order.java
+++ src/main/java/model/dto/Order.java
@@ -1,27 +1,18 @@
 package model.dto;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.Hibernate;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
-@NoArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "order")
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String customer_name;
@@ -31,9 +22,49 @@ public class Order {
     @OneToMany(cascade = {CascadeType.ALL})
     private List<Product> products = new ArrayList<>();
 
+    public Order() {
+    }
 
-    public void addProduct(Product product) {
-        this.products.add(product);
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_phone() {
+        return customer_phone;
+    }
+
+    public void setCustomer_phone(String customer_phone) {
+        this.customer_phone = customer_phone;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customer_name='" + customer_name + '\'' +
+                ", customer_phone='" + customer_phone + '\'' +
+                ", products=" + products +
+                '}';
     }
 
     @Override
@@ -47,15 +78,5 @@ public class Order {
     @Override
     public int hashCode() {
         return getClass().hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", customer_name='" + customer_name + '\'' +
-                ", customer_phone='" + customer_phone + '\'' +
-                ", products=" + products +
-                '}';
     }
 }
--- src/main/java/model/dto/Product.java
+++ src/main/java/model/dto/Product.java
@@ -1,16 +1,10 @@
 package model.dto;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.Hibernate;
 
 import java.util.Objects;
 
-@NoArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "product")
 public class Product {
@@ -23,6 +17,49 @@ public class Product {
     private String description;
     private Double unit_price;
 
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getUnit_price() {
+        return unit_price;
+    }
+
+    public void setUnit_price(Double unit_price) {
+        this.unit_price = unit_price;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "name = " + name + ", " +
+                "description = " + description + ", " +
+                "unit_price = " + unit_price + ")";
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -35,14 +72,5 @@ public class Product {
     @Override
     public int hashCode() {
         return getClass().hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "(" +
-                "id = " + id + ", " +
-                "name = " + name + ", " +
-                "description = " + description + ", " +
-                "unit_price = " + unit_price + ")";
     }
 }

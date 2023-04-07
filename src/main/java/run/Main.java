e08be4c1af349f423db61ae645d1a4651bcc3c58
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-04-07 01:44:00 -0300

    populate the database

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -4,6 +4,7 @@ import view.*;
 
 public class Main {
     public static void main(String[] args) {
+        Populate.main(new String[] {});
     	new TelaMenu();
 //        new TelaFuncionario("Super");
     }

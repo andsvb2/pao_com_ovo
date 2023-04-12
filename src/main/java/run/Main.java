842f1c85b82ace2d4e9d5d572db69b1c944ed06d
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-04-12 18:47:38 -0300

    rename Populate to highlight context and optimize imports

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -1,21 +1,11 @@
 package run;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import model.PcoException;
-import model.dao.OrderDAO;
-import model.dao.ProductDAO;
-import model.dto.Order;
-import model.dto.Product;
 import view.*;
 
 public class Main {
     public static void main(String[] args) {
-        Populate.main(new String[] {});
+        PopulateProducts.main(new String[] {});
     	  new TelaMenu();
           new TelaMenu();
-
-//        new TelaFuncionario("Super");
     }
 }

8ff4faf04a5e9c830c2a810f1bd6d8f422191fc2
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-04-13 04:11:24 -0300

    add constantes para cores

--- src/main/java/utilites/CreateColors.java
+++ src/main/java/utilites/CreateColors.java
@@ -1,2 +1,8 @@
-package utilites;public class CreateColors {
+package utilites;
+
+import java.awt.*;
+
+public class CreateColors {
+    public static final Color MAGNETTA = new Color(255,250,240);
+    public static final Color ORCHID = new Color(160,82,45);
 }

468136fe7b342d0b9bde7b40ac80b5fe90b91943
Anderson S.  Vieira <svieira.anderson@gmail.com>
2023-04-19 01:32:39 -0300

    create method to pay Order on OrderController

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -1,11 +1,13 @@
 package controller;
 
+import model.PcoException;
 import model.dao.OrderDAO;
 import model.dao.OrderItemDAO;
 import model.dao.ProductDAO;
 import model.dto.Order;
 import model.dto.OrderItem;
 import model.dto.Product;
+import model.enums.PaymentStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,10 @@ public class OrderController {
             products.add(orderItem.getProduct());
         }
         return products;
+    }
+
+    public void payOrder(Order order, PaymentStatus paymentStatus) {
+        order.setPaymentStatus(paymentStatus);
     }
 
 }
